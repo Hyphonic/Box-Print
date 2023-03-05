4fc1630dc8b2f929662516a1701a845d5d8cb465
Hyphonical <69310714+Hyphonic@users.noreply.github.com>
2023-03-05 14:29:48 +0100

    Create Box.py

---
+++ Box.py
@@ -0,0 +1,24 @@
+class Box:
+    def __init__(self, Text):
+        self.Text = Text
+        self.Box = []
+        self.Longest = max([len(Line) for Line in self.Text])
+
+        # All The Box Types
+
+        if type(self.Text) == str: # Single Line
+            self.Box.append('╭' + '─' * (len(self.Text) + 2) + '╮')
+            self.Box.append('│ ' + self.Text + ' ' * (len(self.Text) - len(self.Text)) + ' │')
+            self.Box.append('╰' + '─' * (len(self.Text) + 2) + '╯')
+
+        else: # Multiple Lines
+            self.Box.append('╭' + '─' * (self.Longest + 2) + '╮')
+            for Line in self.Text:
+                if Line == '<Sep>':
+                    self.Box.append('├' + '─' * (self.Longest + 2) + '┤')
+                else:
+                    self.Box.append('│ ' + Line + ' ' * (self.Longest - len(Line)) + ' │')
+            self.Box.append('╰' + '─' * (self.Longest + 2) + '╯')
+
+        for Line in self.Box:
+            print(Line)
